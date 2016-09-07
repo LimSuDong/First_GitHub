1600a1714a7d35e6c399255c5e23c95df92e0d75
LimSuDong <limsdong@naver.com>
2016-09-07 09:41:05 +0900

    Day #1 수업
    Day #1 수업(오재홍 교수님)
    - 유니티 프로젝트 만들기
    - MyComponent 연습

---
+++ Assets/Lecture/Scripts/Editor/MyComponent_Editor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MyComponent))] //(MyComponent)를 위한 커스텀 에디터 임을 명시
+public class MyComponent_Editor : Editor
+{
+    void OnEnable()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        MyComponent myComponent = (MyComponent)target; // target이 Object타입이므로 사용할수있게 변환
+
+        myComponent.intVariable =  EditorGUILayout.IntField("Int Variable", myComponent.intVariable);
+        myComponent.floatVariable = EditorGUILayout.Slider("Float Variable", myComponent.floatVariable,0f,100f); // 슬라이더형태
+        myComponent.IntVar = EditorGUILayout.IntField("Int Var", myComponent.IntVar);
+
+        if(GUILayout.Button("Do something") == true) // 눌렀을경우 true
+        {
+            myComponent.DoSomething();
+        }
+    }
+
+}
---
+++ Assets/Lecture/Scripts/MyComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+
+    public int intVariable;
+    public float floatVariable;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public int IntVar //함수 내 변수처럼 존재 - 프로퍼티(Property)
+    {
+        get // read only
+        {
+            return intVariable;
+        }
+        set // white only
+        {
+            intVariable = value;
+        }
+    }
+
+    public void DoSomething()
+    {
+        intVariable++;
+    }
+}
